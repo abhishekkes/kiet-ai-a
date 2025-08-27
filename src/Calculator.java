20e5637dccd8e8e1fed1434e8a237e6f9beea487
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-08-27 09:33:28 +0530

    Inheritance 27/08/25

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x,y,z=0;
+        System.out.println("Enter any two number");
+        x=sc.nextInt();
+        y=sc.nextInt();
+        try
+        {
+            z=x/y;
+            System.out.println("Exception not Occurs");
+        }
+
+        catch(ArithmeticException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        finally {
+            System.out.println(z);
+        }
+
+    }
+}
