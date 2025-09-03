c83389b505a208ddcd92119ae7a913834bbba7b8
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-09-03 10:10:16 +0530

    01/09/25 Collection

--- src/Unit3/ArrayList1.java
+++ src/Unit3/ArrayList1.java
@@ -14,6 +14,5 @@ public class ArrayList1 {
             System.out.println(alist);
         }
 
-
     }
 }
---
+++ src/Unit3/ArrayList3.java
@@ -0,0 +1,33 @@
+package Unit3;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+public class ArrayList3 {
+    public static void main(String[] args) {
+        ArrayList<String> products=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no of products");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            products.add(sc.next());
+        }
+        Iterator itr=products.iterator();
+        while (itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+        products.add(3,"Papaya");
+        products.addLast("Lichi");
+        products.addFirst("Apple");
+
+//        Collections.sort(products);
+        System.out.println(products);
+//        System.out.println(products.getFirst());
+//        System.out.println(products.getLast());
+//        System.out.println(products.get(2));
+    }
+}
